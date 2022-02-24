57e381ed91c07be51c73c8755d3a1ac702d287e6
Ever <ever.leandros91@gmail.com>
2022-02-24 15:19:52 -0500

    Add list component structure

---
+++ src/components/list/Button.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <span class="v-btn__content">
+      <slot name="default"></slot>
+    </span>
+  </div>
+</template>
+
+<script lang="ts">
+import Common from "@/mixin/common";
+import { Component, Mixins } from "vue-property-decorator";
+
+@Component
+export default class EList extends Mixins(Common) {}
+</script>
+<style lang="scss">
+@import "./list.scss";
+</style>
