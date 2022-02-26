9a471345122d38108284b4b445f8c01740291113
Ever <ever.leandros91@gmail.com>
2022-02-25 21:40:00 -0500

    fix: field ripple element

--- vue.config.js
+++ vue.config.js
@@ -6,12 +6,12 @@ module.exports = {
           @import "styles/reset.scss";
           @import "styles/override.scss";
           @import "styles/_mixins.scss";
+          @import "styles/main.scss";
           @import "styles/transitions.scss";
           @import "styles/animations.scss";
           @import "styles/field.scss";
           @import "styles/helper.scss";
           @import "styles/icon.scss";
-          @import "styles/main.scss";
           @import "styles/messages.scss";
           @import "styles/typography.scss";
         `,
